f90227d8bdc7249da0a6383bc12c091f77e78ca2
FND <fnd@fnd-oda.(none)>
2010-09-06 12:05:30 +0200

    fixed packaging
    guarding against generated DESC file missing from the distribution
    
    Having a README different from long_description is proving to be a bit
    of a pain...

--- setup.py
+++ setup.py
@@ -11,18 +11,25 @@ from setuptools import setup, find_packages
 from spackager import __version__
 
 
+try:
+	long_desc = open(os.path.join(os.path.dirname(__file__), 'DESC')).read()
+except IOError: # XXX: hacky way to avoid including DESC in distribution
+	long_desc = None
+
+
 setup(
     name = NAME,
     version = __version__,
     description = DESCRIPTION,
-    long_description = open(os.path.join(os.path.dirname(__file__), 'DESC')).read(),
+    long_description = long_desc,
     author = AUTHOR,
     author_email = AUTHOR_EMAIL,
     url = 'http://pypi.python.org/pypi/%s' % NAME,
     platforms = 'Posix; MacOS X; Windows',
     packages = find_packages(exclude=['test']),
     scripts = ['spac'],
-    install_requires = ['pyquery']
+    #package_data = { '': ['DESC'] },
+    install_requires = ['pyquery'],
     #include_package_data = True,
     #zip_safe = False
 )
